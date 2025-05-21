0cedda3ae7eb09fdc3e74faad4a86db3fd0a2893
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-05-21 21:40:53 +0530

    Update Exercise_4.cpp

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -1,19 +1,65 @@
 #include<stdlib.h> 
 #include<stdio.h> 
+#include <vector>
+using namespace std;
   
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) 
+void merge(int arr[], int left, int mid, int right) 
 { 
     //Your code here
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    
+    vector<int> L(n1);
+    vector<int> R(n2);
+    
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 } 
   
 /* l is for left index and r is right index of the 
    sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r) 
+void mergeSort(int arr[], int left, int right) 
 { 
     //Your code here
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
 } 
   
 /* UTILITY FUNCTIONS */
