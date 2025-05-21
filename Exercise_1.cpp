1a6556b2e221855672443fb114508b1b9e2b6813
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-05-21 16:35:35 +0530

    Update Exercise_1.cpp

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,11 +1,28 @@
-#include <stdio.h> 
-  
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : -
+// Any problem you faced while coding this : -
+
 // A recursive binary search function. It returns 
 // location of x in given array arr[l..r] is present, 
 // otherwise -1 
 int binarySearch(int arr[], int l, int r, int x) 
 {   
     //Your Code here 
+    while(l <= r){
+        int mid = l + (r-l) /2;
+        if(x == arr[mid]){
+            return mid;
+        }
+        
+        if(x < arr[mid]){
+            r = mid - 1;
+        }else {
+            l = mid + 1;
+        }
+    }
+    
+    return -1;
 } 
   
 int main(void) 
